adef0a476261470761d864cf08de1853e53d30db
BuysDB <git@buysdb.nl>
2019-05-16 14:53:52 +0200

    Added preprocessing module

---
+++ toverboom/preprocessing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import networkx as nx
+
+"""
+Convert from '(1,2)' to (1,2) keys
+"""
+def convertFromStringKeys(graph):
+    mapping = {}
+    for key in graph:
+    clone,tp = key.replace('(','').replace(')','').split(',')
+    clone = int(clone)
+    tp = int(tp)
+    mapping[key] = (clone, tp)
+    return nx.relabel_nodes(graph,mapping)
