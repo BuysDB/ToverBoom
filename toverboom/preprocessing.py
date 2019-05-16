411ed6118482826e3edd6d641bc6bd1854102eb7
BuysDB <git@buysdb.nl>
2019-05-16 14:58:17 +0200

    Indentation fix

--- toverboom/preprocessing.py
+++ toverboom/preprocessing.py
@@ -9,8 +9,8 @@ Convert from '(1,2)' to (1,2) keys
 def convertFromStringKeys(graph):
     mapping = {}
     for key in graph:
-    clone,tp = key.replace('(','').replace(')','').split(',')
-    clone = int(clone)
-    tp = int(tp)
-    mapping[key] = (clone, tp)
+        clone,tp = key.replace('(','').replace(')','').split(',')
+        clone = int(clone)
+        tp = int(tp)
+        mapping[key] = (clone, tp)
     return nx.relabel_nodes(graph,mapping)
